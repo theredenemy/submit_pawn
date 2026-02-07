60f730905efb236e0278aaa18ddb4dd136bed60a
theredenemy <1234corbin.1234ad@gmail.com>
2026-02-06 21:19:51 -0800

    modified:   .gitignore 	new file:   build.py 	new file:   build_requirements.txt

---
+++ build.py
@@ -0,0 +1,114 @@
+import os
+import sys
+import requests
+import shutil
+import time
+import subprocess
+import pathlib
+
+platform = sys.platform
+maindir = os.getcwd()
+dir = None
+compiler = None
+archive = None
+plugins_dir = os.path.join(maindir, "plugins")
+scripting_dir = os.path.join(maindir, "scripting")
+
+def log_subprocess(pipe):
+    for line in iter(pipe.readline, b''):
+        print(">", line)
+
+
+
+def download_file(url, filename):
+    file_data = requests.get(url, allow_redirects=True)
+    open(filename, 'wb').write(file_data.content)
+    return filename
+
+# includes
+# dependencies\sm-json\addons\sourcemod\scripting\include\json
+sm_json = os.path.join(maindir, "dependencies", "sm-json", "addons", "sourcemod", "scripting", "include")
+SteamWorks = os.path.join(maindir, "dependencies", "SteamWorks", "Pawn", "includes")
+
+# SourcePawn Script
+
+script = os.path.join(scripting_dir, "submit_pawn.sp")
+
+if not os.path.isdir(sm_json) or not os.path.isdir(SteamWorks):
+    sys.exit(1)
+print("sm-json :", sm_json)
+print("SteamWorks :", SteamWorks)
+print(platform)
+
+if platform == "win32":
+    dir = os.path.join(maindir, "sourcemod-win")
+    compiler = os.path.join(dir, "addons", "sourcemod", "scripting", "spcomp.exe")
+    if not os.path.isfile(compiler) and os.path.isdir(dir):
+        print("Delete :", dir)
+        time.sleep(20)
+        shutil.rmtree(dir)
+    if not os.path.isdir(dir):
+        print("Downloading SourceMod Windows")
+        archive = os.path.join(maindir, "sourcemod-win.zip")
+        os.mkdir(dir)
+        download_file("https://sm.alliedmods.net/smdrop/1.12/sourcemod-1.12.0-git7221-windows.zip", archive)
+        if not os.path.isfile(archive):
+            sys.exit(1)
+        print(f"Unpacking Archive {archive}")
+        shutil.unpack_archive(archive, dir)
+elif platform == "linux":
+    dir = os.path.join(maindir, "sourcemod-linux")
+    compiler = os.path.join(dir, "addons", "sourcemod", "scripting", "spcomp")
+    if not os.path.isfile(compiler) and os.path.isdir(dir):
+        print("Delete :", dir)
+        time.sleep(20)
+        shutil.rmtree(dir)
+    if not os.path.isdir(dir):
+        print("Downloading SourceMod Linux")
+        archive = os.path.join(maindir, "sourcemod-linux.zip")
+        os.mkdir(dir)
+        download_file("https://sm.alliedmods.net/smdrop/1.12/sourcemod-1.12.0-git7221-linux.tar.gz", archive)
+        if not os.path.isfile(archive):
+            sys.exit(1)
+        print(f"Unpacking Archive {archive}")
+        shutil.unpack_archive(archive, dir)
+else:
+    print("Failed")
+    sys.exit(1)
+
+# Does Compiler exist?
+
+if not os.path.isfile(compiler):
+    print("Failed No Compiler")
+    sys.exit(1)
+
+# Compile Plugin
+if not os.path.isdir(plugins_dir):
+    os.mkdir(plugins_dir)
+plugin = os.path.join(plugins_dir, f"{pathlib.Path(script).stem}.smx")
+cmd = subprocess.Popen([compiler, script, f"-o{plugin}", f"-i{sm_json}", f"-i{SteamWorks}", f"-i{os.path.join(scripting_dir, "include")}", f"-i{scripting_dir}", f"-i{os.path.join(dir, "include")}"], text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+while cmd.poll() is None:
+    
+    line = cmd.stdout.readline()
+
+    if line:
+        print(line)
+
+print(cmd.returncode)
+if not cmd.returncode == 0:
+    print("Failed Complie")
+    sys.exit(1)
+else:
+    sys.exit(0)
+
+
+
+
+    
+
+    
+
+
+
+
+
